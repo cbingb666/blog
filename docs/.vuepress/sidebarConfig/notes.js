da4b06bf14d15b48dabebe21d83e73c26a67788d
gs_mac <947694498@qq.com>
2021-05-14 17:39:12 +0800

    update 自动化部署 doc

--- docs/.vuepress/sidebarConfig/notes.js
+++ docs/.vuepress/sidebarConfig/notes.js
@@ -1,1 +1,1 @@
-module.exports = ["Mysql","SQL语法","charles","docker","editorconfig","git命令","html","jenkins","linux","mongodb","nginx","npm","nvm","pm2","ssh config","stylelint","styles","vscode 配置setting.json","vuepress","移动端","移动端真机调试","自动化部署"]
+module.exports = ["Mysql","SQL语法","charles","docker","editorconfig","git命令","html","jenkins","linux","mongodb","nginx","npm","nvm","pm2","ssh config","stylelint","styles","vscode 配置setting.json","vue3+vite","vuepress","移动端","移动端真机调试","自动化部署"]
