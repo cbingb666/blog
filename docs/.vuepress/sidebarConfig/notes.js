b516859c9f7f0dc656540346fad1d774a8a79da9
cbh <947694498@qq.com>
2021-05-22 17:09:53 +0800

    修正 “自动化部署.md” 错误

--- docs/.vuepress/sidebarConfig/notes.js
+++ docs/.vuepress/sidebarConfig/notes.js
@@ -1,1 +1,1 @@
-module.exports = ["Mysql","SQL语法","charles","docker","editorconfig","git命令","html","jenkins","linux","mongodb","nginx","npm","nrm","nvm","pm2","ssh config","stylelint","styles","vscode 配置setting.json","vue3+vite","vuepress","移动端","移动端真机调试","第三方小程序代开发","自动化部署"]
+module.exports = ["Mysql","RegExp","SQL语法","charles","docker","editorconfig","git命令","html","jenkins","linux","mongodb","nginx","npm","nrm","nvm","pm2","ssh config","stylelint","styles","vscode 配置setting.json","vue3+vitejs","vuepress","移动端","移动端真机调试","第三方小程序代开发","自动化部署"]
